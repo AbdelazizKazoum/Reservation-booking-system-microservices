14a334c2db47cfeff6bf40e153ac70e80a318970
Abdelaziz kazoum <abdukazoum@gmail.com>
2025-01-26 14:08:04 +0100

    connect to db

---
+++ starter/libs/common/src/common.module.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
+
+@Module({
+  providers: [],
+  exports: [],
+  imports: [DatabaseModule, ConfigModule],
+})
+export class CommonModule {}
---
+++ starter/libs/common/src/config/config.module.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import {
+  ConfigService,
+  ConfigModule as NestConfigModule,
+} from '@nestjs/config';
+
+@Module({
+  imports: [NestConfigModule.forRoot()],
+  providers: [ConfigService],
+  exports: [ConfigService],
+})
+export class ConfigModule {}
